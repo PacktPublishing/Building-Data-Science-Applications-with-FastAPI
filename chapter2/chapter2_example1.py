4a63043620db21d9671212eb83acdebf053e70ed
François Voron <fvoron@gmail.com>
2021-03-03 17:37:14 +0100

    Add basic Python setup

---
+++ chapter2/chapter2_example1.py
@@ -0,0 +1,3 @@
+l = [1, 2, 3, 4, 5, 6]
+doubles = [i * 2 for i in l]
+assert doubles == [2, 4, 6, 8, 10, 12]
---
+++ tests/test_chapter2.py
@@ -0,0 +1,5 @@
+import runpy
+
+
+def test_chapter2_example1():
+    runpy.run_module("chapter2.chapter2_example1")
