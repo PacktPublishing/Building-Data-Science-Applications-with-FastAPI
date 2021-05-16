13b9cf25d9d06a11e3fe93f50e7dd508ee36253f
François Voron <fvoron@gmail.com>
2021-05-16 17:46:11 +0200

    Add tests for CSRF app example

---
+++ tests/test_chapter7_csrf.py
@@ -0,0 +1,198 @@
+import os
+from datetime import datetime, timedelta
+
+import httpx
+import pytest
+from fastapi import status
+from tortoise import Tortoise
+
+from chapter7.csrf.app import app
+from chapter7.csrf.models import (
+    AccessToken,
+    AccessTokenTortoise,
+    UserDB,
+    UserTortoise,
+)
+from chapter7.csrf.password import get_password_hash
+
+DATABASE_FILE_PATH = "chapter7_csrf.test.db"
+DATABASE_URL = f"sqlite://{DATABASE_FILE_PATH}"
+
+
+@pytest.fixture(autouse=True, scope="module")
+async def initialize_database():
+    await Tortoise.init(
+        db_url=DATABASE_URL,
+        modules={"models": ["chapter7.csrf.models"]},
+    )
+    await Tortoise.generate_schemas()
+
+    initial_users = [
+        UserDB(
+            id=1, email="user1@example.com", hashed_password=get_password_hash("foobar")
+        ),
+    ]
+    await UserTortoise.bulk_create(
+        (UserTortoise(**user.dict()) for user in initial_users)
+    )
+
+    initial_tokens = [
+        AccessToken(
+            access_token="VALID_USER1_TOKEN",
+            user_id=1,
+            expiration_date=datetime.utcnow() + timedelta(seconds=86400),
+        ),
+        AccessToken(
+            access_token="EXPIRED_USER1_TOKEN",
+            user_id=1,
+            expiration_date=datetime.utcnow() - timedelta(seconds=86400),
+        ),
+    ]
+    await AccessTokenTortoise.bulk_create(
+        (AccessTokenTortoise(**token.dict()) for token in initial_tokens)
+    )
+
+    yield
+
+    await Tortoise.close_connections()
+    os.remove(DATABASE_FILE_PATH)
+
+
+@pytest.mark.fastapi(app=app, run_lifespan_events=False)
+@pytest.mark.asyncio
+class TestChapter7CSRFRegister:
+    async def test_invalid_payload(self, client: httpx.AsyncClient):
+        response = await client.post("/register", json={})
+
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+    async def test_email_already_exists(self, client: httpx.AsyncClient):
+        response = await client.post(
+            "/register", json={"email": "user1@example.com", "password": "foobar"}
+        )
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        json = response.json()
+        assert json["detail"] == "Email already exists"
+
+    async def test_valid_payload(self, client: httpx.AsyncClient):
+        response = await client.post(
+            "/register", json={"email": "user2@example.com", "password": "foobar"}
+        )
+
+        assert response.status_code == status.HTTP_201_CREATED
+        json = response.json()
+        assert json["email"] == "user2@example.com"
+        assert "id" in json
+        assert "hashed_password" not in json
+
+
+@pytest.mark.fastapi(app=app, run_lifespan_events=False)
+@pytest.mark.asyncio
+class TestChapter7CSRFLogin:
+    async def test_invalid_payload(self, client: httpx.AsyncClient):
+        response = await client.post("/login", data={})
+
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+    async def test_invalid_email(self, client: httpx.AsyncClient):
+        response = await client.post(
+            "/login",
+            data={"email": "user3@example.com", "password": "invalid_password"},
+        )
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    async def test_invalid_password(self, client: httpx.AsyncClient):
+        response = await client.post(
+            "/login",
+            data={"email": "user1@example.com", "password": "invalid_password"},
+        )
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    async def test_valid_credentials(self, client: httpx.AsyncClient):
+        response = await client.post(
+            "/login",
+            data={"email": "user1@example.com", "password": "foobar"},
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+        assert "token" in response.cookies
+
+
+@pytest.mark.fastapi(app=app, run_lifespan_events=False)
+@pytest.mark.asyncio
+class TestChapter7CSRFGetMe:
+    async def test_not_authenticated(self, client: httpx.AsyncClient):
+        response = await client.get("/me")
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    async def test_invalid_token(self, client: httpx.AsyncClient):
+        response = await client.get("/me", cookies={"token": "INVALID_TOKEN"})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    async def test_expired_token(self, client: httpx.AsyncClient):
+        response = await client.get("/me", cookies={"token": "EXPIRED_USER1_TOKEN"})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    async def test_valid_token(self, client: httpx.AsyncClient):
+        response = await client.get("/me", cookies={"token": "VALID_USER1_TOKEN"})
+
+        assert response.status_code == status.HTTP_200_OK
+        json = response.json()
+
+        assert json == {"id": 1, "email": "user1@example.com"}
+
+
+@pytest.mark.fastapi(app=app, run_lifespan_events=False)
+@pytest.mark.asyncio
+class TestChapter7CSRFUpdateMe:
+    async def test_no_csrf(self, client: httpx.AsyncClient):
+        response = await client.post("/me", json={})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    async def test_not_authenticated(self, client: httpx.AsyncClient):
+        await self._handle_csrf(client)
+        response = await client.post("/me", json={})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    async def test_invalid_token(self, client: httpx.AsyncClient):
+        await self._handle_csrf(client)
+        response = await client.post("/me", cookies={"token": "INVALID_TOKEN"}, json={})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    async def test_expired_token(self, client: httpx.AsyncClient):
+        await self._handle_csrf(client)
+        response = await client.post(
+            "/me", cookies={"token": "EXPIRED_USER1_TOKEN"}, json={}
+        )
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    async def test_valid_token(self, client: httpx.AsyncClient):
+        await self._handle_csrf(client)
+        response = await client.post(
+            "/me",
+            cookies={"token": "VALID_USER1_TOKEN"},
+            json={"email": "user1+updated@example.com"},
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+        json = response.json()
+
+        assert json == {"id": 1, "email": "user1+updated@example.com"}
+
+    async def _handle_csrf(self, client: httpx.AsyncClient):
+        response = await client.get("/csrf")
+        set_cookie_header = response.headers["set-cookie"]
+        csrf = set_cookie_header.split(";")[0].split("=")[1]
+
+        client.cookies["csrftoken"] = csrf
+        client.headers["x-csrftoken"] = csrf
