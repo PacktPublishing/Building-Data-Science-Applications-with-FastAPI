2b0594798c1a7b580a994fdd0114e3ca120d45d8
François Voron <fvoron@gmail.com>
2021-07-27 09:15:18 +0200

    Fix Chapter 14 unit tests by ignoring faces output order

--- tests/test_chapter14.py
+++ tests/test_chapter14.py
@@ -5,6 +5,7 @@ import httpx
 import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
+from pytest_unordered import unordered
 from starlette.testclient import WebSocketTestSession
 
 from chapter14.chapter14_api import app as chapter14_api_app
@@ -31,7 +32,8 @@ class TestChapter14API:
 
         assert response.status_code == status.HTTP_200_OK
         json = response.json()
-        assert json == {"faces": [[237, 92, 80, 80], [426, 75, 115, 115]]}
+        faces = json["faces"]
+        assert unordered(faces) == [[237, 92, 80, 80], [426, 75, 115, 115]]
 
 
 @pytest.mark.fastapi(app=chapter14_websocket_face_detection_app)
@@ -43,7 +45,8 @@ class TestChapter14WebSocketFaceDetection:
             with open(people_image_file, "rb") as image:
                 websocket.send_bytes(image.read())
                 result = websocket.receive_json()
-                assert result == {"faces": [[237, 92, 80, 80], [426, 75, 115, 115]]}
+                faces = result["faces"]
+                assert unordered(faces) == [[237, 92, 80, 80], [426, 75, 115, 115]]
 
     def test_backpressure(self, websocket_client: TestClient):
         QUEUE_LIMIT = 10
@@ -56,4 +59,5 @@ class TestChapter14WebSocketFaceDetection:
                     websocket.send_bytes(bytes)
                 for _ in range(QUEUE_LIMIT):
                     result = websocket.receive_json()
-                    assert result == {"faces": [[237, 92, 80, 80], [426, 75, 115, 115]]}
+                    faces = result["faces"]
+                    assert unordered(faces) == [[237, 92, 80, 80], [426, 75, 115, 115]]
